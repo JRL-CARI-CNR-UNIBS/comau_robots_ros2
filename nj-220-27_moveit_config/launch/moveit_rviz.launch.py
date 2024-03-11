d9c5ed8765f61c5f5ae30a17cb9fd02e9622cf2f
MichiDelle <michi.delle07@gmail.com>
2024-03-11 15:13:22 +0100

    moveit_configs renamed moveit_config

--- nj-220-27_moveit_config/launch/moveit_rviz.launch.py
+++ nj-220-27_moveit_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("nj-220-27", package_name="nj-220-27_moveit_configs").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("nj-220-27", package_name="nj-220-27_moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
